3300e2987d268e4285d3072abd182f071468db5e
Erik Lönroth <erik.lonroth@gmail.com>
2015-01-25 18:47:04 +0100

    Adding MiniUI1
    First checkin of code

---
+++ MiniUI1/src/com/example/miniui1/DebugActivity.java
@@ -0,0 +1,119 @@
+package com.example.miniui1;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.Button;
+// import android.net.Uri;
+
+public class DebugActivity extends Activity {
+	private final String CLASSTAG = "DEBUG_ACTIVITY";
+
+	// Check connectivity: 
+	// http://stackoverflow.com/questions/4101331/editing-or-creating-a-custom-web-page-not-available
+	private Context ctxt;
+	private boolean isConnected = true;
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		ctxt = this;
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_network_test);
+		
+		addListenerOnButton();
+		
+		// Set isConnected, used to act on no-network-state
+		setConnectedState();
+	}
+
+	// Manipulate 'isConnected' accordin to network status
+	private void setConnectedState() {
+		Log.d(CLASSTAG, "setConnectedState entered.");
+		
+		ConnectivityManager connectivityManager = (ConnectivityManager)
+				ctxt.getSystemService(Context.CONNECTIVITY_SERVICE);
+		
+		Log.d(CLASSTAG, "ConnectivityManager assigned in setConnectedState.");
+
+		if (connectivityManager != null) {
+			try {
+				
+				NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+				Log.d(CLASSTAG, "calling isAvailable().");
+				Log.d(CLASSTAG, String.valueOf( info.isAvailable() ) );
+				Log.d(CLASSTAG, "calling isConnected().");
+				Log.d(CLASSTAG, String.valueOf( info.isConnected() ) );
+				Log.d(CLASSTAG, "calling getState()");
+				Log.d(CLASSTAG, info.getState().toString() );
+				Log.d(CLASSTAG, "all States checked...");				
+				String c = info.getState().toString();
+				Log.d(CLASSTAG, c );
+				
+			   if (info.getState() != NetworkInfo.State.CONNECTED) {
+				   // record the fact that there is not connection
+				   Log.d(CLASSTAG, "NetworkInfo.State.CONNECTED seems CONNECTED.");
+				   isConnected = false;
+			   } else {
+				   Log.d(CLASSTAG, String.format("The network state is: NetworkInfo.State.%s", c));
+				   isConnected = true;
+			   }
+		    } catch (Exception e) {
+		    	isConnected = false;
+		    	Log.e("connectivity", e.toString());
+		    }
+		}
+	}
+	
+	public void addListenerOnButton() {
+		 
+		Button button = (Button) findViewById(R.id.button_gettestimg);
+ 
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Log.d(CLASSTAG, "button button_gettestimg pressed");
+				
+				//Get default shared preferences (where our server url is)
+				SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( getApplicationContext() );
+				
+				WebView webView = (WebView) findViewById(R.id.webView_testimg);
+			    webView.getSettings().setJavaScriptEnabled(false);
+			    
+			    // Get settings for server
+			    String url_from_settings = (String) prefs.getString("server_url", "null");
+				String summary;
+				
+			    if (! isConnected ) {
+					summary = "<html><body>You dont have a network.</body></html>";
+					webView.loadData(summary, "text/html", null);
+			    }
+			    else {
+					try { 
+						URL url = new URL(url_from_settings); 
+						webView.loadUrl( url.toString() );
+					}
+					catch ( MalformedURLException e) { 
+						summary = "<html><body>You have a missconfigured server URL in your settings.</body></html>";
+						webView.loadData(summary, "text/html", null);
+					}
+			    }
+				
+				// startActivity(browserIntent);
+			}
+ 
+		});
+	}
+}
---
+++ MiniUI1/src/com/example/miniui1/Home.java
@@ -0,0 +1,119 @@
+package com.example.miniui1;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+// Button http://stackoverflow.com/questions/7570575/onclick-inside-fragment-called-on-activity
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_serversettings) {
+        	// Intent for the Settings
+        	Intent settingsintent = new Intent(this, SettingsActivity.class);
+        	startActivity(settingsintent);
+        
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment implements OnClickListener {
+    	private final String CLASSTAG = "HOME_PLACEHOLDER_FRAGMENT";
+    	
+        public PlaceholderFragment() {
+        
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_navigation, container, false);
+            
+            Button cvb = (Button) rootView.findViewById(R.id.button_checkvideo);
+            Button npb = (Button) rootView.findViewById(R.id.button_newproject);
+            Button mpb = (Button) rootView.findViewById(R.id.button_manageprojects);
+            Button slpb = (Button) rootView.findViewById(R.id.button_startlastproject);
+            
+            cvb.setOnClickListener(this);
+            npb.setOnClickListener(this);
+            mpb.setOnClickListener(this);
+            slpb.setOnClickListener(this);
+            
+            
+            if ( true ) {
+            	Button db = (Button) rootView.findViewById(R.id.button_debug);
+            	db.setOnClickListener(this);
+            }
+            
+            return rootView;
+        }
+
+        // Add onClick
+		@Override
+		public void onClick(View v) {
+			// Switch to right button code based on id.
+			switch (v.getId()) {
+	        case R.id.button_checkvideo:
+	        	Log.d(CLASSTAG, "button_checkvideo pressed");
+	        	break;
+	        case R.id.button_manageprojects:
+	        	Log.d(CLASSTAG, "button_manageprojects pressed");
+	        	break;
+	        case R.id.button_newproject:
+	        	Log.d(CLASSTAG, "button_newproject pressed");
+	        	break;
+	        case R.id.button_startlastproject:
+	        	Log.d(CLASSTAG, "button_startlastproject pressed");
+	        	break;
+	        case R.id.button_debug:
+	        	Log.d(CLASSTAG, "button_networktest pressed");
+	        	Intent networktestintent = new Intent(getActivity(), DebugActivity.class);
+	        	startActivity(networktestintent);
+	        	break;
+	        default:
+	        	Log.e(CLASSTAG, "The impossible has happened!");
+	        	break;
+			}
+		}
+   }
+}
