3cf86599d9eb5caf8c27b0a69b68b7acce605ab3
Erik Lönroth <erik.lonroth@gmail.com>
2015-02-16 14:56:02 +0100

    This fixes: #3

--- MiniUI1/src/com/example/miniui1/GlobalApplication.java
+++ MiniUI1/src/com/example/miniui1/GlobalApplication.java
@@ -55,8 +55,13 @@ public class GlobalApplication extends Application {
 	
 	// Try populate the "allProjects" with projects found on disk
 	private boolean populateProjects() {
+        File[] project_dirs;
 		File appbasepath = getExternalFilesDir(null);
-		File[] project_dirs = appbasepath.listFiles();
+        try {
+            project_dirs = appbasepath.listFiles();
+        } catch (NullPointerException e) {
+            return false;
+        }
 		for ( File pdir : project_dirs ) {
 			String pfn = pdir.toString() + "/project.json";
 			java.io.File projectfile = new java.io.File(pfn);
@@ -64,18 +69,18 @@ public class GlobalApplication extends Application {
 				Log.d("GLOBAL", String.format("Project file found: %s", pdir.getName() ) );
 				Gson gson = new Gson();
 				try {
-					BufferedReader br = new BufferedReader( new FileReader(pfn));  
+					BufferedReader br = new BufferedReader( new FileReader(pfn));
 					//convert the json string back to object
-					
-					
+
+
 					Project projectObj = gson.fromJson(br, Project.class);
 					Log.d("GLOBAL","The project: " + projectObj.toString() );
 					//add it to the list of projects
 					this.addProject( projectObj );
-					} 
+					}
 				catch ( JsonSyntaxException jse ) {
 					Log.e("GLOBAL", String.format("Project file has JsonSyntaxException issues: %s", pdir.getName() ) );
-					jse.printStackTrace();
+                    jse.printStackTrace();
 				}
 				catch ( JsonIOException jioe ) {
 					Log.e("GLOBAL", String.format("Project file has JsonIOException issues: %s", pdir.getName() ) );
